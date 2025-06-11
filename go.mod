8ccc1b99e2c1c745bb8e88c4c2fb0498346ad470
Justin Lamp <justin.lamp@netways.de>
2025-06-11 13:42:17 +0200

    [Authentication] Refactor, fix issues with OIDC
    Signed-off-by: Justin Lamp <justin.lamp@netways.de>

--- go.mod
+++ go.mod
@@ -1,18 +1,18 @@
 module github.com/modzilla99/osssh
 
-go 1.22.3
+go 1.24.3
 
 require (
-	github.com/gophercloud/gophercloud/v2 v2.0.0
-	github.com/gophercloud/utils/v2 v2.0.0-20240705071316-780b64d153e3
+	github.com/gophercloud/gophercloud/v2 v2.7.0
+	github.com/gophercloud/utils/v2 v2.0.0-20250606082759-66f28657aaaa
 	github.com/hashicorp/go-uuid v1.0.3
-	golang.org/x/crypto v0.24.0
+	golang.org/x/crypto v0.39.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/gofrs/uuid/v5 v5.2.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/gofrs/uuid/v5 v5.3.2 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- internal/openstack/auth/auth.go
+++ internal/openstack/auth/auth.go
@@ -9,58 +9,194 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/gophercloud/gophercloud/v2"
 	"github.com/gophercloud/gophercloud/v2/openstack"
+	"github.com/gophercloud/gophercloud/v2/openstack/config/clouds"
 	"github.com/gophercloud/utils/v2/openstack/clientconfig"
+	"gopkg.in/yaml.v3"
 )
 
-func Authenticate(o *clientconfig.ClientOpts) (provider *gophercloud.ProviderClient, err error) {
-	provider, err = clientconfig.AuthenticatedClient(context.Background(), o)
-	if err != nil {
-		if err.Error() == "You must provide exactly one of DomainID or DomainName in a Scope with ProjectName" {
-			o.AuthInfo.DomainName = "default"
+type AuthOptions struct {
+	AccessToken      string
+	AuthType         clouds.AuthType
+	AuthOptions      *gophercloud.AuthOptions
+	IdentityProvider string
+}
 
-			return clientconfig.AuthenticatedClient(context.Background(), o)
-		}
-		if os.Getenv("OS_PROTOCOL") == "openid" {
-			return authenticateWithOIDC()
-		}
+func NewAuthOptions(o *clientconfig.ClientOpts) (*AuthOptions, error) {
+	ao, err := clientconfig.AuthOptions(o)
+	if err != nil {
 		return nil, err
 	}
+
+	// Parse clouds.yaml if environment variable is provided
+	var cloud *Cloud
+	if c := os.Getenv("OS_CLOUD"); c != "" {
+		cloud, err = getAuthCloud(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Set authentication params from clouds.yaml and Environment
+	var (
+		authType                      clouds.AuthType
+		identityProvider, accessToken string
+	)
+	if cloud != nil {
+		authType = cloud.AuthType
+		identityProvider = cloud.IdentityProvider
+		accessToken = cloud.AccessToken
+	}
+
+	authTypeEnv := os.Getenv(`OS_AUTH_TYPE`)
+	identityProviderEnv := os.Getenv(`OS_IDENTITY_PROVIDER`)
+	accessTokenEnv := os.Getenv(`OS_ACCESS_TOKEN`)
+
+	// Enviroment variables override config options from file
+	if authTypeEnv != "" {
+		authType = clouds.AuthType(authTypeEnv)
+	}
+	if identityProviderEnv != "" {
+		identityProvider = identityProviderEnv
+	}
+	if accessTokenEnv != "" {
+		accessToken = accessTokenEnv
+	}
+
+	return &AuthOptions{AccessToken: accessToken, AuthOptions: ao, AuthType: authType, IdentityProvider: identityProvider}, nil
+}
+
+const Authv3OidcAccessToken clouds.AuthType = "v3oidcaccesstoken"
+
+func Authenticate(o *clientconfig.ClientOpts) (provider *gophercloud.ProviderClient, err error) {
+	ao, err := NewAuthOptions(o)
+	if err != nil {
+		return nil, err
+	}
+
+	// If neither domain name or domain id are set, set the name to default
+	// Else if both are set, only use domain id
+	if ao.AuthOptions.DomainName == "" && ao.AuthOptions.DomainID == "" {
+		ao.AuthOptions.DomainName = "default"
+	} else if ao.AuthOptions.DomainName != "" && ao.AuthOptions.DomainID != "" {
+		ao.AuthOptions.DomainName = ""
+	}
+
+	if ao.AuthType == Authv3OidcAccessToken {
+		return authenticateWithOIDC(ao)
+	}
+
+	provider, err = openstack.AuthenticatedClient(context.Background(), *ao.AuthOptions)
+	if err != nil {
+		return nil, err
+	}
+
 	return provider, nil
 }
 
+type Cloud struct {
+	Cloud            string           `yaml:"cloud,omitempty" json:"cloud,omitempty"`
+	Profile          string           `yaml:"profile,omitempty" json:"profile,omitempty"`
+	AccessToken      string           `yaml:"access_token,omitempty" json:"access_token,omitempty"`
+	AuthInfo         *clouds.AuthInfo `yaml:"auth,omitempty" json:"auth,omitempty"`
+	AuthType         clouds.AuthType  `yaml:"auth_type,omitempty" json:"auth_type,omitempty"`
+	RegionName       string           `yaml:"region_name,omitempty" json:"region_name,omitempty"`
+	Regions          []clouds.Region  `yaml:"regions,omitempty" json:"regions,omitempty"`
+	IdentityProvider string           `yaml:"identity_provider"`
 
-func authenticateWithOIDC() (provider *gophercloud.ProviderClient, err error) {
-	//identityProvider := "nws-id"
-	identityProvider := os.Getenv("OS_IDENTITY_PROVIDER")
-	//keystoneURL := "https://cloud.netways.de:5000/v3"
-	keystoneURL := strings.TrimSuffix(os.Getenv("OS_AUTH_URL"), "/")
-	// Keystone JWT token
-	token := os.Getenv("OS_ACCESS_TOKEN")
-	// OpenStack Project Domain Name used for creation of scoped token
-	projectDomainName := os.Getenv("OS_PROJECT_DOMAIN_NAME")
-	// OpenStack Project Name used as the project for the token
-	projectName := os.Getenv("OS_PROJECT_NAME")
+	// EndpointType and Interface both specify whether to use the public, internal,
+	// or admin interface of a service. They should be considered synonymous, but
+	// EndpointType will take precedence when both are specified.
+	EndpointType string `yaml:"endpoint_type,omitempty" json:"endpoint_type,omitempty"`
+	Interface    string `yaml:"interface,omitempty" json:"interface,omitempty"`
 
+	// API Version overrides.
+	IdentityAPIVersion string `yaml:"identity_api_version,omitempty" json:"identity_api_version,omitempty"`
+	VolumeAPIVersion   string `yaml:"volume_api_version,omitempty" json:"volume_api_version,omitempty"`
 
-	if identityProvider == "" || keystoneURL == "" || token == "" || projectDomainName == "" || projectName == "" {
-		fmt.Println("Please provide OS_IDENTITY_PROVIDER, OS_AUTH_URL, OS_ACCESS_TOKEN, OS_PROJECT_DOMAIN_NAME and OS_PROJECT_NAME as enviroment variables")
-		return nil, errors.New("authentication impossible")
+	// Verify whether or not SSL API requests should be verified.
+	Verify *bool `yaml:"verify,omitempty" json:"verify,omitempty"`
+
+	// CACertFile a path to a CA Cert bundle that can be used as part of
+	// verifying SSL API requests.
+	CACertFile string `yaml:"cacert,omitempty" json:"cacert,omitempty"`
+
+	// ClientCertFile a path to a client certificate to use as part of the SSL
+	// transaction.
+	ClientCertFile string `yaml:"cert,omitempty" json:"cert,omitempty"`
+
+	// ClientKeyFile a path to a client key to use as part of the SSL
+	// transaction.
+	ClientKeyFile string `yaml:"key,omitempty" json:"key,omitempty"`
+}
+
+func getAuthCloud(cloud string) (authType *Cloud, err error) {
+	const (
+		currentDir = "./clouds.yaml"
+		userPath   = ".config/openstack/clouds.yaml"
+		systemPath = "/etc/openstack/clouds.yaml"
+	)
+	f, err := os.ReadFile(currentDir)
+	if err != nil && !os.IsNotExist(err) {
+		return
+	}
+	if err != nil && os.IsNotExist(err) {
+		var homeDir string
+		homeDir, err = os.UserHomeDir()
+		f, err = os.ReadFile(path.Join(homeDir, userPath))
+		if err != nil && !os.IsNotExist(err) {
+			return
+		}
+		if err != nil && os.IsNotExist(err) {
+			f, err = os.ReadFile(systemPath)
+			if err != nil && !os.IsNotExist(err) {
+				return
+			}
+			if err != nil && os.IsNotExist(err) {
+				return nil, fmt.Errorf("Could not find clouds.yaml")
+			}
+		}
+	}
+
+	type Clouds struct {
+		Clouds map[string]Cloud `yaml:"clouds" json:"clouds"`
+	}
+
+	var clouds Clouds
+	if err := yaml.Unmarshal(f, &clouds); err != nil {
+		return nil, err
+	}
+	c, ok := clouds.Clouds[cloud]
+	if !ok {
+		return nil, fmt.Errorf("Could not find cloud %s, in clouds.yaml", cloud)
+	}
+
+	return &c, nil
+}
+
+func authenticateWithOIDC(authOptions *AuthOptions) (provider *gophercloud.ProviderClient, err error) {
+	if authOptions.IdentityProvider == "" ||
+		authOptions.AuthOptions.IdentityEndpoint == "" ||
+		authOptions.AccessToken == "" ||
+		authOptions.AuthOptions.DomainName == "" ||
+		authOptions.AuthOptions.TenantName == "" {
+		return nil, errors.New("authentication not possbile, please provide all necessary environment/clouds.yaml configuration options")
 	}
 
 	// Issue unscoped token
-	url := fmt.Sprintf("%s/OS-FEDERATION/identity_providers/%s/protocols/openid/auth", keystoneURL, identityProvider)
-	osToken, err := issueUnscopedToken(url, token)
+	url := fmt.Sprintf("%s/OS-FEDERATION/identity_providers/%s/protocols/openid/auth", authOptions.AuthOptions.IdentityEndpoint, authOptions.IdentityProvider)
+	osToken, err := issueUnscopedToken(url, authOptions.AccessToken)
 	if err != nil {
 		return nil, err
 	}
 
 	// use unscoped token for scoped token creation
-	url = keystoneURL + "/auth/tokens"
-	token, catalog, err := issueScopedToken(url, osToken, projectDomainName, projectName)
+	url = authOptions.AuthOptions.IdentityEndpoint + "/auth/tokens"
+	token, catalog, err := issueScopedToken(url, osToken, authOptions.AuthOptions.DomainName, authOptions.AuthOptions.TenantName)
 	if err != nil {
 		return nil, err
 	}
@@ -70,7 +206,7 @@ func authenticateWithOIDC() (provider *gophercloud.ProviderClient, err error) {
 		return nil, err
 	}
 	o.TokenID = token
-	o.DomainName = projectDomainName
+	o.DomainName = authOptions.AuthOptions.DomainName
 
 	// Create provider client with issued token
 	providerClient, err := openstack.NewClient(o.IdentityEndpoint)
@@ -78,7 +214,7 @@ func authenticateWithOIDC() (provider *gophercloud.ProviderClient, err error) {
 		return nil, err
 	}
 	providerClient.TokenID = token
-	mock := func (eo gophercloud.EndpointOpts) (string, error) {
+	mock := func(eo gophercloud.EndpointOpts) (string, error) {
 		for _, e := range catalog {
 			if e.Type != eo.Type {
 				continue
@@ -107,22 +243,22 @@ func issueUnscopedToken(url, bearer string) (string, error) {
 	token := "Bearer " + bearer
 
 	// Create a new HTTP request
-    req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
-        return "", err
-    }
-
-    // Set headers
-    req.Header.Set("Content-Type", "application/json")
-    req.Header.Set("Authorization", token)
-
-    // Create an HTTP client and send the request
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
 		return "", err
-    }
-    defer resp.Body.Close()
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", token)
+
+	// Create an HTTP client and send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 
 	return resp.Header.Get("X-Subject-Token"), nil
 }
@@ -138,7 +274,7 @@ type Catalog []struct {
 	ID   string `json:"id"`
 	Type string `json:"type"`
 	Name string `json:"name"`
-} 
+}
 
 type Token struct {
 	Token struct {
@@ -182,10 +318,10 @@ func issueScopedToken(url, unscopedToken, domain, project string) (string, Catal
 
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
+	resp, err := client.Do(req)
+	if err != nil {
 		return "", nil, err
-    }
+	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
